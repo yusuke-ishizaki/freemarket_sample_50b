2e5aeedb95d590699044b43cc3c395498baa5965
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-15 19:36:29 +0900

    Tyo-se-
    調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   # root 'users#index'
   root 'product#index'
-  get 'product/index'
+  get 'product/product_info'
+  get 'card/index'
   # root 'users#plofile'
   # resources :users, only: [:index, :plofile, :personal_info, :new, :show, :edit]
 
