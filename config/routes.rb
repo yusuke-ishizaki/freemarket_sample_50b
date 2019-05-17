60260042bd372d034af2458ddc2c3efe39703bb1
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-17 13:09:27 +0900

    微調整
    Route action layout 作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   # root 'users#index'
   root 'product#index'
-  get 'product/product_info'
-  get 'card/index'
+  get 'product' => 'product#index'
+  get 'card' => 'card#index'
   # root 'users#plofile'
   # resources :users, only: [:index, :plofile, :personal_info, :new, :show, :edit]
 
