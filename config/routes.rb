ec6f5f2fb4172d500e2b3600552410ffb014b74e
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-17 21:16:09 +0900

    change3
    routing変更
    Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
 
-  # root 'users#index'
   root 'product#index'
-  get 'product' => 'product#index'
-  get 'product/show' => 'product#show'
-  get 'card' => 'card#index'
-  get 'card/new' => 'card#new'
-  # root 'users#plofile'
-  # resources :users, only: [:index, :plofile, :personal_info, :new, :show, :edit]
+  resources :product, only: [ :index, :show]  
+  resources :card, only: [:index, :new]
 
 
 end
