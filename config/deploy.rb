783eacdcea021e3e1a2bbc48eff483e512aad349
Tatsuya3030 <prelude.wonderfuldream30@gmail.com>
2019-05-17 17:51:28 +0900

    デプロイファイル記述追加
    -環境変数の追加

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,8 @@ end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"],
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
