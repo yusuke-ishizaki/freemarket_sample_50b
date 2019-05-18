0c6db84fa2e4dd79a60eea8f91ee13e42c195911
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-18 20:39:08 +0900

    モデル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_092353) do
+ActiveRecord::Schema.define(version: 2019_05_18_113832) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code"
@@ -45,9 +45,10 @@ ActiveRecord::Schema.define(version: 2019_05_18_092353) do
     t.string "name"
     t.text "text"
     t.integer "price"
-    t.integer "status"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
