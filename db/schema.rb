77d8af211bcc18220023582500c58e1a4f771301
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-18 20:43:05 +0900

    モデル修正
    Userテーブルにname追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_113832) do
+ActiveRecord::Schema.define(version: 2019_05_18_114214) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_05_18_113832) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
