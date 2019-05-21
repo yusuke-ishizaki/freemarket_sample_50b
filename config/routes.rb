837b03342e7921d6a5a0e6464e37459f894ea4fb
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-21 12:03:01 +0900

    微修正
    削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'product#index'
-<<<<<<< HEAD
   get   '/users_product/:id', to: 'product#users_product'
   resources :product
-=======
-  resources :product, only: [ :index, :show]
->>>>>>> master
   resources :card, only: [:index, :new]
 
 
