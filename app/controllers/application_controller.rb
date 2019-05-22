bf216e65815735d45c7b2eddd43b48d84a3c6fce
Tatsuya3030 <prelude.wonderfuldream30@gmail.com>
2019-05-22 13:49:55 +0900

    ユーザー新規登録
    -サーバーサイドのユーザー新規登録
    -ファイル整理
    -デバイスコントローラ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'product#index'
+  resources :users
   get   '/users_product/:id', to: 'product#users_product'
   resources :product
   resources :card, only: [:index, :new]
