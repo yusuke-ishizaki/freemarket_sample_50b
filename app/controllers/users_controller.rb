c46995108e232ad62b7675b7346a440caf61ea48
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-23 17:16:40 +0900

    修正
    side_bar
    ユーザーマイページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @product = Product.all
+    @product_count = Product.where(user_id: current_user.id).count
   end
 
   def personal_info
