6aa55a511dd823a25243a0e0f24004cbaf28caf1
suzukinan <tateos.study.only@gmail.com>
2019-05-23 21:07:25 +0900

    routes 調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
-  # root 'product#index'
-  # resources :users
-  # get   '/users_product/:id', to: 'product#users_product'
-  # resources :product
-  # get '/logout' => 'users#logout'
+  devise_for :users
+  root 'product#index'
+  resources :users
+  get   '/users_product/:id', to: 'product#users_product'
+  resources :product
+  get '/logout' => 'users#logout'
 
-  # resources :card, only: [:new, :show, :index] do
-  #   collection do
-  #     post 'show', to: 'card#show'
-  #     post 'pay', to: 'card#pay'
-  #     post 'delete', to: 'card#delete'
-  #   end
-  # end
+  resources :card, only: [:new, :show, :index] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 
-  # resources :purchase, only: [:index] do
-  #   collection do
-  #   get 'index', to: 'purchase#index'
-  #   post 'pay', to: 'purchase#pay'
-  #   get 'done', to: 'purchase#done'
-  #   get 'confrm', to: 'purchase#confirm'
-  #   end
-  # end
+  resources :purchase, only: [:index] do
+    collection do
+    get 'index', to: 'purchase#index'
+    post 'pay', to: 'purchase#pay'
+    get 'done', to: 'purchase#done'
+    get 'confrm', to: 'purchase#confirm'
+    end
+  end
 
 
-get "/product/product_info"
+# get "/product/product_info"
+
 end
