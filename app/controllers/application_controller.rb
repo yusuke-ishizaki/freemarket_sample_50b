efd0b62ee2598a87ad86e198fdcd2a3fb8560ea7
suzukinan <tateos.study.only@gmail.com>
2019-05-24 18:37:59 +0900

    微調整　修正
    ログアウト後のpath修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-  def after_sign_out_path_for(resource)
-    root_path # ログアウト後に遷移するpathを設定
-  end
+  # def after_sign_out_path_for(resource)
+  #   root_path # ログアウト後に遷移するpathを設定
+  # end
 
   protected
 
