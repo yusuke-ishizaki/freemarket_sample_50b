94b463e633db0539671707413b3388cef716765a
suzukinan <tateos.study.only@gmail.com>
2019-05-24 18:34:47 +0900

    微調整　registrations/new.html.haml
    Root_pathを設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :authenticate_user!
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+
   protected
 
   def configure_permitted_parameters
