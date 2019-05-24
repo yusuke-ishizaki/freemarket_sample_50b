a40f10991124034f748312a3d2d82b86c6ee3c9a
kanbekmito <hideyuki-kreva@hotmail.co.jp>
2019-05-24 16:22:21 +0900

    eddit to change
    vew の微調整と
    出品機能の実装部修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,24 +9,38 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create!(params.require(:item).permit(:content, images:[])) # POINT
-    redirect_to new_item_path
+    if @item.save
+      redirect_to new_item_path
+    else
+      render :new
+    end
   end
 
   def show
-    @item = Item.find(params[:id])
+    set_item
   end
 
   def edit
-    @item = Item.find(params[:id])
+    set_item
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update params.require(:item).permit(:content, :images) # POINT
-    redirect_to @item
+    if @item.user_id == current_user.id
+      @item.update(item_params)
+      flash[:notice] = "商品情報を編集しました"
+      redirect_to item_confirmation_items_path(@item)
+    else
+      flash[:notice] = "権限がありません"
+      redirect_to  edit_item_path
+    end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def create_params
     # images以外の値についてのストロングパラメータの設定
     item_params = params.require(:item).permit(:content)
