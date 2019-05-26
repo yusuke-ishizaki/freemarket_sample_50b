2b87df0232ff04736d1db8c848de073b16ce6ed5
Tatsuya3030 <prelude.wonderfuldream30@gmail.com>
2019-05-26 13:15:24 +0900

    失敗定義不要
    -余計な定義を削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -21,10 +21,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-
-  def failure
-    redirect_to root_path and return
-  end
 end
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
