93b700c17e78cc4caba8319abe63d7f131d22546
suzukinan <tateos.study.only@gmail.com>
2019-05-28 11:52:59 +0900

    微調整　_top_header.html.haml
    新規会員登録・ログイン
    リンク先を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@
 require 'csv'
 
 CSV.foreach('db/category.csv', headers: true) do |row|
-  Content.create(
+  Content.create!(
     id: row['id'],
     name: row['name'],
     parent_id: row['parent_id'],
