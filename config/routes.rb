e5edd2e6d24685cc36c50c6d4ca3a5c30300caa8
suzukinan <tateos.study.only@gmail.com>
2019-05-28 12:41:32 +0900

    微調整　ビュ崩れ　product_status.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,10 @@ Rails.application.routes.draw do
   resources :users
   get   '/users_product/:id', to: 'product#users_product'
   get   '/product_status/:id', to: 'product#product_status'
-  get '/logout' => 'users#logout'
-  get '/users/show/123' => 'users#show'
+  get  '/logout' => 'users#logout'
+  get  '/users/show/123' => 'users#show'
+  get  '/product_status', to: 'product#product_status'
+
 
   resources :card, only: [:new, :show, :index] do
     collection do
