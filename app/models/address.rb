77a002bd14e50470d64f304bb0786203b167fe11
yusuke-ishizaki <isizaki1114@gmail.com>
2019-05-28 21:48:04 +0900

    change
    修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_085723) do
+ActiveRecord::Schema.define(version: 2019_05_28_124138) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_085723) do
     t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "prefecture_id"
     t.index ["product_id"], name: "index_deliveries_on_product_id"
   end
 
