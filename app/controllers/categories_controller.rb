3128e7c46fb8eafec118731b75f36156924dfd17
suzukinan <tateos.study.only@gmail.com>
2019-05-30 16:15:25 +0900

    修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,18 +4,7 @@ class CategoriesController < ApplicationController
 
     @parents = Category.where(parent_id: 0)
 
-    # @parents = Category.find(params[:id])
-
-    # @child = Category.where(parent_id: 1)
-    
-
-    # @ladis = @parents.find(1)
-    # binding.pry
-
-    # @categories = Category.eager_load(children: :children).where(parent_id: nil)
-
-    # @categories = Category.where(parent_id: nil)
-
+    # 以下はメモです
     # @category = Category.find(1)
     # # 下記でレディースに紐付く子カテゴリーであるトップス、パンツ、靴を全て取得出来る
     # @category.children
@@ -35,24 +24,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    # 一覧には際最新の20件が表示されるようにする記述
-    # @products = Product.all.order(created_at: :desc).limit(20)
-
-    # children = Category.where(parent_id: 1)
     @category = Category.find(params[:id])
     @products = Product.order(created_at: :desc).where(category_id: @category.id).limit(20)
-    binding.pry
-
-    # @products = Product.order(created_at: :desc).where(params[:category_id]).limit(20)
-
-
-    # params idを使用してID取得
-    # category.products
-
-    # @child = Category.where(parent_id: 1)
-
-    # @category = Category.find(1)
-    # @category = Category.select('name')
-    # @category_children_name = Category.select()
   end
 end
